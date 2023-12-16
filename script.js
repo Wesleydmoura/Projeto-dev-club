4f0255700fe9c8df8a5ccabc10e208b42a4bb78c
Wesleydmoura <Wdmoura93@outlook.com>
2023-12-16 19:38:31 -0300

    Codigo alterado

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const convertButton = document.querySelector(".convert-button")
 
-function convertValues() {
+const convertButton = document.querySelector("convert-button")
+const currencySelect = document.querySelector("currency-select")
+const currencyValue= document.querySelector ("currency-value")
+
+function convertValues( event) {
     const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
+    console.log(" currencySelect")
 
     const dolarToday = 5.2
-    const convertedValue = inputCurrencyValue / dolarToday
 
-    console.log(convertedValue)
+    const euroToday = 6.2
+    if (currencySelect.value == "dolar") {
+
+        currencyValueConverted.innerHTML == new Intl.NumberFormat("en-US" ,{
+            style: "currency",
+            currency: "USD",
+
+        }
+        ).format(inputCurrencyValuE / dolarToday)
+    }
 }
 convertButton.addEventListener("click", convertValues)
+
+
